93a5a9b5046d07b523be3ec16034ef891bdac0d0
Andrew Cox <andrew@lunarg.com>
2023-11-06 22:19:04 +0000

    Union - Decoded_D3D12_RENDER_TARGET_VIEW_DESC JSON
    Union field of Decoded_D3D12_RENDER_TARGET_VIEW_DESC converted
    to JSON depending on the ViewDimension enum field.

--- framework/generated/generated_dx12_struct_decoders_to_json.cpp
+++ framework/generated/generated_dx12_struct_decoders_to_json.cpp
@@ -2347,7 +2347,60 @@ void FieldToJson(nlohmann::ordered_json& jdata, const Decoded_D3D12_RENDER_TARGE
         const Decoded_D3D12_RENDER_TARGET_VIEW_DESC& meta_struct = *data;
         FieldToJson(jdata["Format"], decoded_value.Format, options); // Basic data plumbs to raw struct [is_enum]
         FieldToJson(jdata["ViewDimension"], decoded_value.ViewDimension, options); // Basic data plumbs to raw struct [is_enum]
-        ; ///< @todo ALERT: Union member 0 of D3D12_RENDER_TARGET_VIEW_DESC needs special handling.
+        switch(decoded_value.ViewDimension)
+        {
+            case D3D12_RTV_DIMENSION_UNKNOWN:
+            {
+                FieldToJson(jdata["Warning"], "Zero D3D12_RTV_DIMENSION in D3D12_RENDER_TARGET_VIEW_DESC. Uninitialised struct?", options);
+                break;
+            }
+            case D3D12_RTV_DIMENSION_BUFFER:
+            {
+                FieldToJson(jdata["Buffer"], meta_struct.Buffer, options);
+                break;
+            }
+            case D3D12_RTV_DIMENSION_TEXTURE1D:
+            {
+                FieldToJson(jdata["Texture1D"], meta_struct.Texture1D, options);
+                break;
+            }
+            case D3D12_RTV_DIMENSION_TEXTURE1DARRAY:
+            {
+                FieldToJson(jdata["Texture1DArray"], meta_struct.Texture1DArray, options);
+                break;
+            }
+            case D3D12_RTV_DIMENSION_TEXTURE2D:
+            {
+                FieldToJson(jdata["Texture2D"], meta_struct.Texture2D, options);
+                break;
+            }
+            case D3D12_RTV_DIMENSION_TEXTURE2DARRAY:
+            {
+                FieldToJson(jdata["Texture2DArray"], meta_struct.Texture2DArray, options);
+                break;
+            }
+            case D3D12_RTV_DIMENSION_TEXTURE2DMS:
+            {
+                FieldToJson(jdata["Texture2DMS"], meta_struct.Texture2DMS, options);
+                break;
+            }
+            case D3D12_RTV_DIMENSION_TEXTURE2DMSARRAY:
+            {
+                FieldToJson(jdata["Texture2DMSArray"], meta_struct.Texture2DMSArray, options);
+                break;
+            }
+            case D3D12_RTV_DIMENSION_TEXTURE3D:
+            {
+                FieldToJson(jdata["Texture3D"], meta_struct.Texture3D, options);
+                break;
+            }
+            default:
+            {
+                FieldToJson(jdata["Warning"], "Unknown D3D12_RTV_DIMENSION in D3D12_RENDER_TARGET_VIEW_DESC. Corrupt struct?", options);
+                FieldToJson(jdata["Unknown value"], uint32_t(decoded_value.ViewDimension), options);
+                break;
+            }
+        }
     }
 }
 
